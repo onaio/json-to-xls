bb7f90e8e12ad8b57af296f7ff450e865d53f5cc
Kiran <kirang20@gmail.com>
2013-11-04 12:26:26 +0530

    Kiran | #729 | Now able to generate XLS and return it without having to save the excel.

--- src/main/java/io/ei/jsontoxls/resources/JsonToXlsResource.java
+++ src/main/java/io/ei/jsontoxls/resources/JsonToXlsResource.java
@@ -2,6 +2,7 @@ package io.ei.jsontoxls.resources;
 
 import io.ei.jsontoxls.core.Employee;
 import net.sf.jxls.transformer.XLSTransformer;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -37,18 +39,17 @@ public class JsonToXlsResource {
         staff.add(new Employee("Oleg", 32, 2300, 0.25));
         staff.add(new Employee("Neil", 34, 2500, 0.00));
         staff.add(new Employee("Maria", 34, 1700, 0.15));
-        staff.add(new Employee("John", 35, 2800, 0.20));
+        staff.add(new Employee("John", 36, 2800, 0.20));
         Map beans = new HashMap();
         beans.put("employee", staff);
         XLSTransformer transformer = new XLSTransformer();
         try {
-            transformer.transformXLS("employees.xls", beans, "build/employees-out.xls");
+            FileInputStream inputStream = new FileInputStream("employees.xls");
 
-            FileInputStream stream = new FileInputStream("build/employees-out.xls");
-            byte[] excelBytes = new byte[stream.available()];
-            stream.read(excelBytes);
-
-            return Response.ok(getOut(excelBytes)).build();
+            Workbook workbook = transformer.transformXLS(inputStream, beans);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            workbook.write(outputStream);
+            return Response.ok(getOut(outputStream.toByteArray())).build();
         } catch (Exception e) {
             logger.error("XLS Transformation failed. Exception: " + e);
             return Response.status(503).build();
