ee287504d06f5bf899bb7d7176304620a3a59ed0
Thirunavukkarasu <thirunar@thoughtworks.com>
2013-11-07 13:42:44 +0530

    Thiru |#735| Added manifest file to jar

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ ssh prod '\
     export time=`date +%F_%T`; \
     mkdir -p old/backup-json-to-xls.${time} && \
     ([[ `ls | grep -q "json-to-xls.*"; echo $?` = 0 ]] && /bin/mv -f json-to-xls* old/backup-json-to-xls.${time} || true) && \
-    wget http://nexus.motechproject.org/content/repositories/json-to-xls/io/ei/jsontoxls/json-to-xls/0.2-SNAPSHOT/json-to-xls-0.2-20131106.124454-1.jar && \
-    java -jar json-to-xls-0.2-20131106.124454-1.jar'
+    wget http://nexus.motechproject.org/content/repositories/json-to-xls/io/ei/jsontoxls/json-to-xls/0.2-SNAPSHOT/json-to-xls-0.2-20131107.062824-2.jar && \
+    java -jar json-to-xls-0.2-20131107.062824-2.jar'
