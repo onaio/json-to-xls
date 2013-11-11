5f01e6b5716ed5def68cad1db319746726d57bc7
Thirunavukkarasu <thirunar@thoughtworks.com>
2013-11-11 15:31:05 +0530

    Thiru & Kumaran | #735 | Uploading standalone jar to nexus

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-VERSION="$1"
 [[ -z "$1" ]] && echo "Usage: $0 version-of-json-to-xls-delivery-to-deploy" >&2 && exit 1
 
+VERSION=$1
+FILENAME="json-to-xls-${VERSION}.jar"
+echo "FETCHING FILE: ${FILENAME}"
 ssh prod '\
     export time=`date +%F_%T`; \
     mkdir -p old/backup-json-to-xls.${time} && \
     ([[ `ls | grep -q "json-to-xls.*"; echo $?` = 0 ]] && /bin/mv -f json-to-xls* old/backup-json-to-xls.${time} || true) && \
-    wget http://nexus.motechproject.org/content/repositories/json-to-xls/io/ei/jsontoxls/json-to-xls/0.2-SNAPSHOT/json-to-xls-0.2-20131106.124454-1.jar && \
-    java -jar json-to-xls-0.2-20131106.124454-1.jar'
+    wget http://nexus.motechproject.org/content/repositories/json-to-xls/io/ei/jsontoxls/json-to-xls/0.2-SNAPSHOT/'${FILENAME}' && \
+    unzip -d json-to-xls '${FILENAME}' && \
+    java -jar '${FILENAME}'` server json-to-xls/json-to-xls.yml'
