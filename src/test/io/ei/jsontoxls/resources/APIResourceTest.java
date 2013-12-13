4fd216474029ccaf845bd186580fd57aab849513
Kiran <kirang20@gmail.com>
2013-12-13 11:05:31 +0530

    Kiran | #784 | Fixed the test for API documentation.

--- src/test/io/ei/jsontoxls/resources/APIResourceTest.java
+++ src/test/io/ei/jsontoxls/resources/APIResourceTest.java
@@ -10,8 +10,10 @@ public class APIResourceTest {
     public void shouldReturnTheAPIDetails() throws Exception {
         String expectedDetails = "POST /templates, with excel template file as payload.\n" +
                 "    This saves the template and returns a unique token for that template in the response. Henceforth this token can be used for referring this template.\n" +
-                "POST /xls/<token> with JSON data as payload.\n" +
-                "    This will use the template corresponding to the token in the URL and generate an Excel file using the template and JSON data. This generated Excel file is returned in the response.";
+                "POST /xls/<template_token> with JSON data as payload.\n" +
+                "    This will use the template corresponding to the token in the URL and generate an Excel file using the template and JSON data. The URL using which this excel file can be downloaded is returned in the response.\n" +
+                "GET /xls/<generated_excel_token>\n" +
+                "    This returns the excel file for given token.";
 
         assertEquals(expectedDetails, new APIResource("api.txt").details().getEntity());
     }
