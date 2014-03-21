6377c680d6aa62b31f080c016b83c78bf76a7610
sowmyas <sm.sowmya14@gmail.com>
2014-03-21 10:07:40 +0530

    Sowmya | #926 | Fixing the build.

--- src/test/io/ei/jsontoxls/resources/APIResourceTest.java
+++ src/test/io/ei/jsontoxls/resources/APIResourceTest.java
@@ -10,6 +10,8 @@ public class APIResourceTest {
     public void shouldReturnTheAPIDetails() throws Exception {
         String expectedDetails = "POST /templates, with excel template file as payload.\n" +
                 "    This saves the template and returns a unique token for that template in the response. Henceforth this token can be used for referring this template.\n" +
+                "PUT /templates/<template-token> with excel template file as payload. This updates the existing the template with the new template that is sent as the payload and returns the template token \n" +
+                "GET /templates/<template-token> This fetches the corresponding template for the given excel template token\n" +
                 "POST /xls/<template_token> with JSON data as payload.\n" +
                 "    This will use the template corresponding to the token in the URL and generate an Excel file using the template and JSON data. The URL using which this excel file can be downloaded is returned in the response.\n" +
                 "GET /xls/<generated_excel_token>\n" +
